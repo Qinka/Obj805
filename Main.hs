6a4498dcbcf6519f9d429041e4556c7a059835a2
Qinka <qinka@live.com>
2016-02-03 15:38:49 +0800

    update

--- Main.hs
+++ Main.hs
@@ -23,27 +23,77 @@ module Main
         { dc :: TVar Int
         }
 
-      instance Yesod Obj
+      instance Yesod Obj where
+        defaultLayout w = do
+          pc <- widgetToPageContent w
+          withUrlRenderer [hamlet|
+            $newline never
+            $doctype 5
+            <html>
+              <head>
+                #{pageTitle pc}
+                <meta charset=utf-8>
+                <meta name=viewport content=width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no>
+                ^{pageHead pc}
+              <body>
+                ^{pageBody pc}
+            |]
 
       mkYesod "Obj" [parseRoutes|
       / HomeR GET
-      /get-dc DcR GET
-      /set/#Int SetR GET
+      /get DcR GET
+      /set SetR GET
       |]
 
       getHomeR :: Handler Html
-      getHomeR = defaultLayout [whamlet|???|]
+      getHomeR = do
+        (Obj d) <- getYesod
+        speed <- lookupGetParams "speed"
+        case speed of
+          s:_ -> do
+            let i = read $ read $ show s
+            liftIO $ atomically $ writeTVar d i
+            return ()
+          _-> return ()
+        defaultLayout[whamlet|
+          当前速度：
+          <iframe src=get frameborder=0 height=25>
+          <form name=input action="" method=get>
+            速度
+            <br>
+            0
+            <input type=radio name=speed value=0>
+            <br>
+            25
+            <input type=radio name=speed value=25>
+            <br>
+            50
+            <input type=radio name=speed value=50>
+            <br>
+            75
+            <input type=radio name=speed value=75>
+            <br>
+            100
+            <input type=radio name=speed value=100>
+            <br>
+            <input type=submit value=更新 />
+          |]
 
       getDcR :: Handler Text
       getDcR = do
         (Obj d) <- getYesod
         liftIO $ fmap (pack.show) $ readTVarIO d
 
-      getSetR :: Int -> Handler Html
-      getSetR i = do
+      getSetR :: Handler Html
+      getSetR  = do
         (Obj d) <- getYesod
-        liftIO $ atomically $ writeTVar d i
-        return "Changed"
+        speed <- lookupGetParams "speed"
+        case speed of
+          s:_ -> do
+            let i = read $ read $ show s
+            liftIO $ atomically $ writeTVar d i
+            return "Changed"
+          _ -> return "UnChanged"
 
       main :: IO ()
       main = do
