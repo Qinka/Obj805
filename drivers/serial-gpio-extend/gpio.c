4c89b816839c93a2cebd18911712429865c99afd
Qinka <qinka@live.com>
2017-07-02 00:40:13 +0800

    rewrite gpio

---
+++ drivers/serial-gpio-extend/gpio.c
@@ -0,0 +1,34 @@
+/*************************************************************************************
+ * 
+ *   The driver for serial-gpio-extend (for output)
+ *   This device uses shift registers, such as 74HC595, to extend gpio.
+ *   For a normal device, we can use 3 pins to ``extend'' to 16 pins.
+ *
+ *   Copyright (C) Qinka 2017
+ *   Licence: GPLv3+
+ *   Maintainer: me@qinka.pro
+ *   Stability: experimental
+ *   Portability: just for raspberrypi(2B) (using BCM 2835)
+ *
+ *   GPIO source file
+ *   
+ **************************************************************************************
+ */
+
+#include "gpio.h"
+
+static void set_gpio_function(struct bcm_2835_gpio_o*gpio, int pin, int function) {
+  int reg_index = pin / 10;
+  int bit = (pin % 10) *3;
+  unsigned old = gpio->GPFSEL[reg_index];
+  unsigned mask = 0b111 << bit;
+  printk(KERN_DEBUG "GPIO: changing function of GPIO pin %d from %x to %x\n",pin, (old >> bit) & 0b111,function);
+  gpio->GPFSEL[reg_index] = (old & ~mask) | ((function << bit) & mask);
+}
+
+static void set_gpio_output_val(struct bcm_2835_gpio_o*gpio, int pin, int val) {
+  if (val != 0)
+    gpio->GPSET[pin / 32] |= 1 << (pin % 32);
+  else
+    gpio->GPSET[pin / 32] &= ~(1 << (pin % 32));
+}
---
+++ drivers/serial-gpio-extend/gpio.h
@@ -0,0 +1,39 @@
+/*************************************************************************************
+ * 
+ *   The driver for serial-gpio-extend (for output)
+ *   This device uses shift registers, such as 74HC595, to extend gpio.
+ *   For a normal device, we can use 3 pins to ``extend'' to 16 pins.
+ *
+ *   Copyright (C) Qinka 2017
+ *   Licence: GPLv3+
+ *   Maintainer: me@qinka.pro
+ *   Stability: experimental
+ *   Portability: just for raspberrypi(2B) (using BCM 2835)
+ *
+ *   GPIO Header file
+ *   
+ **************************************************************************************
+ */
+
+
+#include <mach/platform.h>
+
+#ifndef _GPIO_H_
+#define _GPIO_H_
+
+
+
+// struct for gpio
+
+struct bcm2835_gpio_o {
+  u32 GPFSEL[6];
+  u32 __reserved_1;
+  u32 GPSET[2];
+  u32 __reserved_2;
+  u32 GPCLR[2];
+};
+
+static void set_gpio_function(int pin, int function);
+static void set_gpio_output_val(int pin, int val);
+
+#endif // _GPIO_H_
