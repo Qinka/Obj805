5087ac52c8dfced15fdfb3383c4fde31305fc70c
Qinka <qinka@live.com>
2017-07-01 23:33:01 +0800

    for v-bis-m

---
+++ drivers/serial-gpio-extend/serial-gpio-extend.c
@@ -0,0 +1,79 @@
+/*************************************************************************************
+ * 
+ *   The driver for serial-gpio-extend (for output)
+ *   This device uses shift registers, such as 74HC595, to extend gpio.
+ *   For a normal device, we can use 3 pins to ``extend'' to 16 pins.
+ *
+ *   Copyright (C) Qinka 2017
+ *   Licence: GPLv3+
+ *   Maintainer: me@qinka.pro
+ *   Stability: experimental
+ *   Portability: just for raspberrypi(2B) (using BCM 2835)
+ *
+ *   Header file
+ *   
+ **************************************************************************************
+ */
+
+
+
+#include "serial-gpio-extend.h"
+
+
+
+// global variables
+static struct serial_gpio_extend_output* sgeo_entry = NULL;
+static struct dev_t sgeo_dev; // device
+static int count;
+static int sgeo_major = 0;
+static int pins[3] = {0,0,0};
+static int gpio_bits = 0;
+
+
+// open operation
+static int sgeo_open(struct inode *inode, struct file* filp) {
+  printk(KERN_INFO "SGEO: Open, %d\n",count);
+  if(sqeo_entry == NULL) {
+    if (count == 0)
+      printk(KERN_WARNING "SGEO: count is not zero, but sqeo_entry is NULL\n"); 
+    sgeo_entry = kmalloc(sizeof(struct serial_gpio_extend_outpu),GFP_KERNEL);
+    if (sgeo_entry == NULL) {
+      printk(KERN_ERR "SGEO: can not allocate memory\n");
+      return -ENOMEM;
+    }
+    sgeo_entry->data_pin = DATA_PIN(pins);
+    sgeo_entry->sync_pin = SYNC_PIN(pins);
+    sgeo_entry->clk_pin  = CLK_PIN(pins);
+    if (gpio_bits)
+      sgeo_entry->gpio_bits = gpio_bits;
+    else
+      sgeo_entry->gpio_bits = BITS_SIZE;
+    sgeo_entry->dev_no = inode->i_rdev;
+    sgeo_entry->gpio_buffer = kmalloc(BUFFER_SZIE(sgeo_entry->gpio_bits),GFP_KERNEL);
+    init_rwsem(sgeo_entry->sem);
+    sgeo_entry->cdev = inode->i_cdev;
+    file->private_data = sgeo_entry;
+  }
+  try_module_get(THIS_MODULE);
+  ++count;
+  return 0;
+}
+    
+    
+// close operation
+static int sgeo_close(struct inode *inode, struct file *filp){
+  -- count;
+  printk(KERN_INFO "SEGO: Close, %d\n",count);
+  if(count == 0) {
+    if(sego_entry != NULL)
+      kfree(sego_entry->gpio_buffer);
+    else
+      printk(KERN_WARNING "SEGO: sego_entry is NULL\n");
+    kfree(sego_entry);
+    sego_entry = NULL;
+  }
+  module_put(THIS_MODULE);
+  return 0;
+}
+    
+   
---
+++ drivers/serial-gpio-extend/serial-gpio-extend.h
@@ -0,0 +1,76 @@
+/*************************************************************************************
+ * 
+ *   The driver for serial-gpio-extend (for output)
+ *   This device uses shift registers, such as 74HC595, to extend gpio.
+ *   For a normal device, we can use 3 pins to ``extend'' to 16 pins.
+ *
+ *   Copyright (C) Qinka 2017
+ *   Licence: GPLv3+
+ *   Maintainer: me@qinka.pro
+ *   Stability: experimental
+ *   Portability: just for raspberrypi(2B) (using BCM 2835)
+ *
+ *   Header file
+ *   
+ **************************************************************************************
+ */
+
+#ifndef _SERIAL_GPIO_EXTEND_H_
+#define _SERIAL_GPIO_EXTEND_H_
+
+// headers
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/time.h>
+#include <linux/jiffies.h>
+#include <linux/rwsem.h>
+#include <linux/errno.h>
+
+// struct for serial gpio extend
+struct serial_gpio_extend_output {
+  /* data pin is used to translate the single bit
+   * for a byte 7 - - - - - - 0
+   *                          |
+   *                          first bit
+   */
+  int   data_pin;
+  /* sync pin is used to output what is in the registers
+   * after translated all bits, this bit should be high for a moment(then be low).
+   */
+  int   sync_pin;
+  /* clk signal
+   */
+  int   clk_pin;
+  /* how many bit the hardware has
+   */
+  int   gpio_bits;
+  /* dev_t
+   */
+  dev_t dev_no;
+  /* buffer for char(8bits)
+   */
+  char* gpio_buffer;
+  /* the semaphore
+   */
+  struct rw_semaphpore sem;
+  struct cdev* cdev;
+};
+
+// default & configure
+/* default bits
+ */
+#define BITS_SIZE (8)
+/* how many byte need to hold all bits
+ */
+#define BUFFER_SIZE(b) ((b==0?0:b-1) >> 8) + 1)
+/* get the pins
+ */
+#define DATA_PIN(p) (p[0])
+#define SYNC_PIN(p) (p[1])
+#define CLK_PIN(p)  (p[2])
+
+#endif // _SERIAL_GPIO_EXTEND_H_
