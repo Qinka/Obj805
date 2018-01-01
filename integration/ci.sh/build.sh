d720991c4fa30c50b5f70e2838a6d8b1fd0202d5
Johann Lee <qinka@live.com>
2018-01-01 17:13:37 +0800

    update; fix the errors in script of CI

--- integration/ci.sh/build.sh
+++ integration/ci.sh/build.sh
@@ -5,7 +5,7 @@ set -e
 if [ -n "$RUN_BUILD" ]; then
     ghc -V	
     O2FLAG=" --ghc-options -O3 "
-    if [ -n "$LLVM" ]; then
+    if [ -n "$LLVM" ] ; then
 	    export LLVMFLAG=" --ghc-options -fllvm --ghc-options -pgmlo --ghc-options opt-$LLVM --ghc-options -pgmlc --ghc-options llc-$LLVM "
     fi
     if [ -n "$THREADED" ]; then
--- integration/ci.sh/init.sh
+++ integration/ci.sh/init.sh
@@ -3,30 +3,26 @@
 set -e
 echo pre-install
 
-########
-echo update apt
-sudo apt update
-
 ######
 echo fetch the system\' name
 export OS_CORENAME=$(lsb_release -c | awk '{print $2}')
 export OS_DISTRIBUTOR=$(lsb_release -i | awk '{print $3}')
 echo using $OS_DISTRIBUTOR  $OS_CORENAME
 
-#####
-if [ -n "$STACKSOLVER" ]; then
-    export STACKFILE=" --stack-yaml $TRAVIS_BUILD_DIR/$STACKSOLVER"
-fi
-echo STACKFILE $STACKFILE
+
+########
+echo update apt
+sudo apt update
+
+######
+echo Install ghc-$GHC_VER and cabal-install-$CABAL_VER
+sudo apt install ghc-$GHC_VER cabal-install-$CABAL_VER
 
 ######
 if [ -n "$LLVM" ]; then
   echo install llvm
   wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-  echo deb http://apt.llvm.org/$OS_CORENAME/ llvm-toolchain-$OS_CORENAME main | sudo tee -a /etc/apt/sources.list.d/llvm.list
-  echo deb-src http://apt.llvm.org/$OS_CORENAME/ llvm-toolchain-$OS_CORENAME main | sudo tee -a /etc/apt/sources.list.d/llvm.list
   echo deb http://apt.llvm.org/$OS_CORENAME/ llvm-toolchain-$OS_CORENAME-$LLVM main | sudo tee -a /etc/apt/sources.list.d/llvm.list
-  echo deb-src http://apt.llvm.org/$OS_CORENAME/ llvm-toolchain-$OS_CORENAME-$LLVM main | sudo tee -a /etc/apt/sources.list.d/llvm.list
   sudo apt update
   sudo apt install -y libllvm$LLVM libllvm$LLVM-dbg liblldb-$LLVM lldb-$LLVM llvm-$LLVM llvm-$LLVM-dev llvm-$LLVM-runtime
 else
@@ -52,3 +48,9 @@ travis_retry curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wild
 echo config haskell-stack
 stack config set system-ghc --global true
 stack path --programs $STACKFILE
+
+#####
+if [ -n "$STACKSOLVER" ]; then
+    export STACKFILE=" --stack-yaml $TRAVIS_BUILD_DIR/$STACKSOLVER"
+fi
+echo STACKFILE $STACKFILE
