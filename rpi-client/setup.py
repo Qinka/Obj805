d66ab5da9751a1796b78b2ef4e02ca13c9eea89b
Johann Lee <qinka@live.com>
2018-01-10 20:31:24 +0800

    update

--- rpi-client/setup.py
+++ rpi-client/setup.py
@@ -1,6 +1,16 @@
 from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='obj805-rpi',
-version='0.1',
-py_modules=['host','extension'],
+setup(
+    name = 'obj805-rpi',
+    version = '0.1',
+    keywords = ('obj805', 'rpi'),
+    description = 'rpi client of obj805',
+    license = 'GPLv3',
+
+    author = 'Johann Lee',
+    author_email = 'me@qinka.pro',
+
+    packages = find_packages(),
+    platforms = 'Linux',
 )
