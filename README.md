0feff72b30e01c2d9e8003dec326a32b80a1d60e
Johann Lee <qinka@live.com>
2018-01-20 11:19:33 +0800

    update the extension.js to get the wave function.

--- README.md
+++ README.md
@@ -25,10 +25,20 @@ function DTFT(sca,coe,value,coe_,bias,delay) {...}
 `coe_` and `bias` is the transformation parameter of hold function.
 `delay` is the update delay(microsecond).
 
-### Example
+### Example (old version with DTFT)
 
-```
+```javascript
 sca = [1,0.5,0.25,0.125,0.0625,0.0313,0.0156,0.0078];
 coe = [0,1,2,3,4,5,6,7];
 DTFT(sca,coe,0,0.7531,-0.5003,100);
+```
+
+### Example (new version with mkSim)
+
+To make a square wave:
+```javascript
+sca=[1,1/3,1/5,1/7];
+coe=[1,3,5,7];
+sq=mkSim(sca,coe,Math.sin);
+timeoutNV(sq,50,50,0,0.1,300);
 ```
--- front-end/library/extension.js
+++ front-end/library/extension.js
@@ -29,3 +29,26 @@ function DTFT(sca,coe,value,coe_,bias,delay) {
   };
   timeoutRedo(function(v){uploadNewValueToSvr(100*(coe_ * sinF(v) + bias));},value,0.1,delay);
 }
+
+/**
+ * Simulate function with func()
+ */
+function mkSim(sca,coe,func) {
+  var len = Math.min(sca.length,coe.length);
+  return function(x) {
+    var sum = 0;
+    for(i=0;i < len; i++)
+    sum += sca[i] * func(coe[i] * x);
+    return sum;
+  }
+}
+
+/**
+ * timeout delay 
+ */
+
+function timeoutNV(func,coe,bias,value,step,delay) {
+  timeoutRedo(function(v){
+    uploadNewValueToSvr(coe * func(v) + bias);},
+  value,step,delay);
+}
